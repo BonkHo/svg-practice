f029a798feb4bc60373d5de8eb2e84ce28dcb632
John Bae <john21225@gmail.com>
2021-12-07 19:39:11 -0800

    SVG image created in react with jsx

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+console.log(ReactDOM);
+
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const eyeOffSetX = 90;
+const eyeOffSetY = 50;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<body>
+				<svg width={width} height={height}>
+					<circle
+						r="200"
+						cx={centerX}
+						cy={centerY}
+						fill="yellow"
+						stroke="black"
+						stroke-width="10"
+					></circle>
+					<circle
+						r="30"
+						cx={centerX - eyeOffSetX}
+						cy={centerY - eyeOffSetY}
+						stroke-width="10"
+					></circle>
+					<circle
+						r="30"
+						cx={centerX + eyeOffSetX}
+						cy={centerY - eyeOffSetY}
+						stroke-width="10"
+					></circle>
+				</svg>
+			</body>
+		</div>
+	);
 }
 
 export default App;
