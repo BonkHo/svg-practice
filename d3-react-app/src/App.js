68e2cf451fa472d6acafb434221c2c7110a84aad
John Bae <john21225@gmail.com>
2021-12-11 19:38:04 -0800

    Face complete

--- d3-react-app/src/App.js
+++ d3-react-app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { arc } from "d3";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const width = 960;
+	const height = 500;
+	const centerX = width / 2;
+	const centerY = height / 2;
+	const eyeOffSetX = 90;
+	const eyeOffSetY = 50;
+	const mouthWidth = 5;
+	const mouthRadius = 120;
+
+	const mouthArc = arc()
+		.innerRadius(mouthRadius)
+		.outerRadius(mouthRadius + mouthWidth)
+		.startAngle(Math.PI / 2)
+		.endAngle((Math.PI * 3) / 2);
+
+	return (
+		<div className="App">
+			<body>
+				<svg width={width} height={height}>
+					<g transform={`translate(${centerX}, ${centerY})`}>
+						<circle
+							r="200"
+							fill="yellow"
+							stroke="black"
+							stroke-width="10"
+						></circle>
+						<circle
+							r="30"
+							cx={-eyeOffSetX}
+							cy={-eyeOffSetY}
+							stroke-width="10"
+						></circle>
+						<circle
+							r="30"
+							cx={+eyeOffSetX}
+							cy={-eyeOffSetY}
+							stroke-width="10"
+						></circle>
+						<path d={mouthArc()} stroke="black" stroke-width="10"></path>
+					</g>
+				</svg>
+			</body>
+		</div>
+	);
 }
 
 export default App;
